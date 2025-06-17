efad1e3437b8cb4493b1a1d65be9924dc150f6fc
hayuraaa <haris.200170154@mhs.unimal.ac.id>
2025-06-17 14:14:34 +0700

    SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="432JcWSePqM6n-WHiVg9ZWfuWEXMqNXjSvJpYcqXmXE" />
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         <link rel="icon" type="image/jpeg" href="{{ asset('favicon.jpg') }}">
